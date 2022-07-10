a317a1dc5516c8d3475e382bc67ac58be7931028
s-potato <51790227+s-potato@users.noreply.github.com>
2022-07-10 10:17:13 +0700

    add level select

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,19 +1,46 @@
+import { useState } from "react";
 import Board from "../Board";
 import Header from "../Header";
 import "./style.css"
 
 function Game() {
-    const height = 9
-    const width = 9
-    const mines = 10
+    const [level, setLevel] = useState('easy')
+
+    const easy = {
+        height: 9,
+        width: 9,
+        mines: 10
+    }
+
+    const normal = {
+        height: 16,
+        width: 16,
+        mines: 40
+    }
+
+    const hard = {
+        height: 16,
+        width: 30,
+        mines: 99
+    }
 
     return (
         <div>
             <Header activeLink={"game"}></Header>
             <div>
-                <h1>Minesweeper</h1>
+                <h1>Minesweeper {level}</h1>
+                <div className="select">
+                    <select value={level} onChange={(e)=>{console.log(level==="easy");setLevel(()=>e.target.value)}}>
+                        <option value={"easy"}>Easy</option>
+                        <option value={"normal"}>Normal</option>
+                        <option value={"hard"}>Hard</option>
+                    </select>
+                </div>
                 <div className="board">
-                    <Board height={height} width={width} mines={mines} />
+                    {level === "easy" && <Board level="easy" height={easy.height} width={easy.width} mines={easy.mines} />}
+                    {level === "normal" && <Board level="normal" height={normal.height} width={normal.width} mines={normal.mines} />}
+                    {level === "hard" && <Board level="hard" height={hard.height} width={hard.width} mines={hard.mines} />}
+                    
                 </div>
             </div>
         </div>
